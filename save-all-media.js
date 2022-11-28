c6a8274a2fb263096d1fdc7917f4b49ff496033e
Ben Suen <ben1one@gmail.com>
2022-11-29 03:34:23 +0800

    update

--- save-all-media.js
+++ save-all-media.js
@@ -6,12 +6,28 @@ const fs = require('fs');
 const https = require('https')
 const md5 = require('md5');
 
-const save = (filename, content) => {
-    fs.writeFile(`output/${filename}`, content.text, { encoding: content.encoding }, function (err) {
+const myArgs = process.argv.slice(2);
+
+const saveFromHar = (filename, content) => {
+    fs.writeFile(filename, content.text, { encoding: content.encoding }, function (err) {
         if (err) throw err;
         console.log(`${filename} saved`)
     });
 }
+
+const download = function (url, dest) {
+    const req = https.get(url, (res) => {
+        const filePath = fs.createWriteStream(dest);
+        res.pipe(filePath);
+        filePath.on('finish', () => {
+            filePath.close();
+            console.log('Download Completed');
+        })
+    })
+    req.on('error', function (err) {
+        throw err;
+    });
+};
 
 const rawdata = fs.readFileSync(harFile);
 const json = JSON.parse(rawdata);
@@ -24,15 +40,27 @@ json.log.entries.forEach(element => {
         _resourceType
     } = element;
 
-    if (mimeType == "video/mp4") {
-        if (typeof text == "string") {
-            const hash = md5(text);
-            save(`${hash}.${mimeType.split(';')[0].split('/').slice(-1)}`, content)
-        } else {
-            console.error("No Content")
-            throw err;
+    // Note the request maybe broken as the video is not fully loaded
+    if(myArgs[0]==="saveFromHar"){
+        if (mimeType == "video/mp4") {
+            if (typeof text == "string") {
+                const hash = md5(text);
+                const filename = `output/${hash}.${mimeType.split(';')[0].split('/').slice(-1)}`;
+                saveFromHar(filename, content)
+            } else {
+                console.error("No Content")
+                throw err;
+            }
         }
+    }
 
+    // Note the url may expire
+    if(myArgs[0]==="downloadFromRemote"){
+        if (mimeType == "video/mp4") {
+            const hash = md5(url);
+            const filename = `output/${hash}.${mimeType.split(';')[0].split('/').slice(-1)}`;
+            download(url, filename, (e) => console.error(e))
+        }
     }
 
 });
